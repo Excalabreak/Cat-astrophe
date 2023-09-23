d7bd25fd5c7563a341c06fd32378997699addc2b
Isademonium <aiherrera@mail.csuchico.edu>
2023-09-22 22:50:34 -0700

    UI

---
+++ Cat-astrophe/Assets/Scripts/UI/Objective.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Objective : MonoBehaviour
+{
+    // Varibles
+    public AudioSource objSFX;
+    public GameObject theObjective;
+
+}
