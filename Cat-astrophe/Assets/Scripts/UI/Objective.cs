b2f72e4ad8d36a3b4e442a37c66bb1436b772997
Isademonium <aiherrera@mail.csuchico.edu>
2023-09-22 23:32:38 -0700

    UI
    Objective system works!

--- Cat-astrophe/Assets/Scripts/UI/Objective.cs
+++ Cat-astrophe/Assets/Scripts/UI/Objective.cs
@@ -8,5 +8,31 @@ public class Objective : MonoBehaviour
     // Varibles
     public AudioSource objSFX;
     public GameObject theObjective;
+    public GameObject theTrigger;
+    public GameObject theText;
 
+
+    // Trigger enter
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(missionObj());
+        }
+
+        // Action
+         IEnumerator missionObj()
+        {
+            objSFX.Play();
+            theObjective.SetActive(true);
+            theObjective.GetComponent<Animation>().Play("ObjectiveDisplayAnim");
+            theText.GetComponent<Text>().text = "Destroy everything befro Clara gets back home!";
+
+            // Delay function
+            yield return new WaitForSeconds(7.3f);
+            theText.GetComponent<Text>().text = "";
+            theTrigger.SetActive(false);
+            theObjective.SetActive(false);
+        }
+    }
 }
