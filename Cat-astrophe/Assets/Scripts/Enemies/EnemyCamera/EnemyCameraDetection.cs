9feeb66578974ff870d5895565c9ea64c732ba5b
Justin Lam <jlam123321@gmail.com>
2023-09-29 00:28:41 -0700

    prefab made for camera

--- Cat-astrophe/Assets/Scripts/Enemies/EnemyCamera/EnemyCameraDetection.cs
+++ Cat-astrophe/Assets/Scripts/Enemies/EnemyCamera/EnemyCameraDetection.cs
@@ -39,7 +39,7 @@ public class EnemyCameraDetection : MonoBehaviour
                 {
                     pd.IsDetected = true;
                     return;
-                    //Debug.Log("see player");
+                    Debug.Log("see player");
                 }
                 else
                 {
