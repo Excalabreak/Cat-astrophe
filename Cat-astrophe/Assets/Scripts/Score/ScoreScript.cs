9cbf70de67e7430df215f64c881e32b20069dc94
Justin Lam <jlam123321@gmail.com>
2023-10-16 19:01:24 -0700

    made cat claws
    will give a damage boost, but has limited durability

--- Cat-astrophe/Assets/Scripts/Score/ScoreScript.cs
+++ Cat-astrophe/Assets/Scripts/Score/ScoreScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ScoreScript : MonoBehaviour
 {
+    /// <summary>
+    /// adds score to score manager
+    /// </summary>
+    /// <param name="s">how many points does the player get</param>
     public void AddScore(int s)
     {
         GameScoreManager.Instance.AddToScore(s);
