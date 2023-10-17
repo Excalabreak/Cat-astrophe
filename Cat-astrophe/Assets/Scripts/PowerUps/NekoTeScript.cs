c5204ecbcfc8bdd75910b9ca46d5312d2d53c0ff
Justin Lam <jlam123321@gmail.com>
2023-10-16 20:08:37 -0700

    this will cause errors, but edited level design scene

--- Cat-astrophe/Assets/Scripts/PowerUps/NekoTeScript.cs
+++ Cat-astrophe/Assets/Scripts/PowerUps/NekoTeScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class NekoTeScript : MonoBehaviour
 {
+    //IDEA TO REMEMGER: REMOVE NEKOTE, REDUCE TIME AND/OR CONE OF SHAME AFTER DESTROYING CAMERA
+    //GIVES DILEMA OF DESTROYING CAMERAS OR BLOCKING THEM
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
