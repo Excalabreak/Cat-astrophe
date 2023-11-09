dff18d4ea9428acbbf99d46a42f8da7305b45bda
Justin Lam <jlam123321@gmail.com>
2023-11-08 17:51:29 -0800

    pickup is done, toss is next

--- Cat-astrophe/Assets/Scripts/Player/Cat Actions/PickUp2.cs
+++ Cat-astrophe/Assets/Scripts/Player/Cat Actions/PickUp2.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class PickUp2 : MonoBehaviour
 {
-    [SerializeField] private InteractTrigger interactTrigger;
-    private GameObject heldItem;
+    [SerializeField] private LayerMask pickUpMask;
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private Transform pickUpTarget;
+
+    [SerializeField] private float pickUpRange;
+    [SerializeField] private GameObject CurrentObject;
+    [SerializeField] private Rigidbody CurrentObjectRigidBody;
+    [SerializeField] private Collider CurrentObjectCollider;
+
+    private bool doPickUp = false;
 
     public void HandleToss()
     {
@@ -14,11 +22,48 @@ public class PickUp2 : MonoBehaviour
 
     public void HandlePickUp()
     {
-
+        doPickUp = !doPickUp;
     }
 
-    public void GetPotentialPickUps(GameObject go)
+    private void Update()
     {
+        if (doPickUp)
+        {
+            Ray CameraRay = playerCamera.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
+            if (Physics.Raycast(CameraRay, out RaycastHit hit, pickUpRange, pickUpMask))
+            {
+                CurrentObject = hit.transform.gameObject;
+                CurrentObjectRigidBody = CurrentObject.GetComponent<Rigidbody>();
+                CurrentObjectCollider = CurrentObject.GetComponent<Collider>();
 
+                CurrentObjectRigidBody.useGravity = false;
+                CurrentObjectCollider.enabled = false;
+            }
+        }
+        else
+        {
+            if (CurrentObject)
+            {
+                CurrentObjectRigidBody.useGravity = true;
+                CurrentObjectCollider.enabled = true;
+
+                CurrentObject = null;
+                CurrentObjectRigidBody = null;
+                CurrentObjectCollider = null;
+
+                return;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (CurrentObject)
+        {
+            Vector3 directionToPoint = pickUpTarget.position - CurrentObjectRigidBody.position;
+            float distanceToPoint = directionToPoint.magnitude;
+
+            CurrentObjectRigidBody.velocity = directionToPoint * 12f * distanceToPoint;
+        }
     }
 }
