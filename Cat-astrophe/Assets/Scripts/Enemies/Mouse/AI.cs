46a2679ac5adee5cd5eb50ed4432457be28e00a1
mar43ly <103211097+mar43ly@users.noreply.github.com>
2023-11-12 18:56:32 -0800

    fix mouse

--- Cat-astrophe/Assets/Scripts/Enemies/Mouse/AI.cs
+++ Cat-astrophe/Assets/Scripts/Enemies/Mouse/AI.cs
@@ -8,15 +8,17 @@ public class AI : MonoBehaviour
     public NavMeshAgent agent;
     public Transform player;
     public LayerMask Ground, Player;
+    public int speed;
 
-    [SerializeField] private float displacementDist = 5f;
+    [SerializeField] private float displacementDist = 3f;
 
     //Patrolling
     public Vector3 walkPoint;
     bool walkPointSet;
     public float walkPointRange;
+    
 
-    //Running Away
+    ////Running Away
     public float timeBetweenRunningAway;
     bool alreadyRunAway;
 
@@ -28,6 +30,7 @@ public class AI : MonoBehaviour
     {
         player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
+       
     }
 
     private void Update()
@@ -39,14 +42,8 @@ public class AI : MonoBehaviour
         if (!playerInSightRange && !playerInAttackRange) Patroling();
         if (playerInSightRange && !playerInAttackRange) Stare();
         if (playerInSightRange && playerInAttackRange) RunAway();
-    }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            Destroy(this.gameObject);
-        }
+        //RunAway();
     }
 
     private void Patroling()
@@ -57,13 +54,13 @@ public class AI : MonoBehaviour
 
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
 
-        //walkpoint reached
+       //walkpoint reached
         if (distanceToWalkPoint.magnitude < 1f) walkPointSet = false;
     }
 
     private void SearchWalkPoint()
     {
-        //calculate random point in range
+       //calculate random point in range
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
 
@@ -74,11 +71,18 @@ public class AI : MonoBehaviour
     
     private void RunAway()
     {
-        Vector3 normDir = (player.position - transform.position).normalized;
+        //Vector3 normDir = (player.position - transform.position).normalized;
 
-        normDir = Quaternion.AngleAxis(Random.Range(0, 179), Vector3.up) * normDir;
+        //normDir = Quaternion.AngleAxis(Random.Range(0, 179), Vector3.up) * normDir;
 
-        agent.Move(transform.position - (normDir * displacementDist));
+        //agent.Move(transform.position - (normDir * displacementDist));
+
+        Vector3 direction = transform.position - player.position;
+        if (direction.sqrMagnitude<25f)
+        {
+            transform.Translate(direction.normalized * speed *Time.deltaTime, Space.World);
+            transform.forward = direction.normalized;
+        }
     }
     
     private void Stare()
