d5709894815a8c6bb2f6a9ac6aced2fcaa24843f
Ana Isabel Herrera Del Rio <145712731+AnaIsa4@users.noreply.github.com>
2023-11-14 10:26:06 -0800

    Mouse Fixed

--- Cat-astrophe/Assets/Scripts/Enemies/Mouse/Kill.cs
+++ Cat-astrophe/Assets/Scripts/Enemies/Mouse/Kill.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Kill : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Scratch")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Scratch")
         {
             Destroy(this.gameObject);
         }
